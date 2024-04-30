91e7be969af52df543b146df86aee63d26f84770
SeleneRoze <athenaseleneluna@gmail.com>
2024-04-30 16:26:30 -0600

    añadí la segunda tarjeta

--- cards2.js
+++ cards2.js
@@ -34,3 +34,41 @@ CARD.append(NAME_SECTION, DECS_SECTION, AGE_SECTION, MUSIC_SECTION);
 
 
 CARDS_CONTAINER.appendChild(CARD);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const CARDS2_CONTAINER = document.getElementById("card2-container");
+const PROFILE = {
+    id: 1,
+    userName: "Username",
+    desc: "Sobre mí",
+    age: 26,
+
+    fav_movie: {
+        movie: ["Perfect Blue","Jennifer´s body","Mean Girls"],
+    },
+
+};
+const card2 = document.createElement("div");
+const NAME_SECTION2 = document.createElement("h3"); 
+const DECS_SECTION2 = document.createElement("p"); 
+const AGE_SECTION2 = document.createElement("p"); 
+const MOVIE_SECTION2 = document.createElement("div"); 
+
+const movielist = PROFILE.fav_movie.movie.map(e => {
+    const item = document.createElement("ul");
+    item.textContent = e;
+    return item;
+});
+
+console.log(movielist);
+
+NAME_SECTION2.textContent = PROFILE.userName; 
+DECS_SECTION2.textContent = PROFILE.desc; 
+AGE_SECTION2.textContent = PROFILE.age; 
+
+MOVIE_SECTION2.append(...movielist);
+
+card2.append(NAME_SECTION2, DECS_SECTION2, AGE_SECTION2, MOVIE_SECTION2); 
+
+CARDS2_CONTAINER.appendChild(card2); 
+
